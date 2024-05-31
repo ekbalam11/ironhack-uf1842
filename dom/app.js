e1f2d53aba5b6cd9339e8b9294e8646e36d5eb40
ekbalam11 <ekbalam11@gmail.com>
2024-05-31 02:13:54 +0200

    bar chart complete, message pending...

--- dom/app.js
+++ dom/app.js
@@ -1,18 +1,15 @@
 // ¿En tu páis de origen es normal dejar propina?
 
-const bill = document.querySelector('#bill-amount').value;
+const bill = document.querySelector('#bill-amount');
 const percetangeTip = document.querySelector('#percetage-tip');
 const totalTip = document.querySelector('#tip-amount');
 const totalBill = document.querySelector('#total');
 const button = document.querySelector('#calculate');
 
-button.addEventListener('click', function () {
-    bill.value= bill + percetangeTip;
-    console.log("Esto tiene la const totalTip: ", totalTip);
-    console.log(typeof totalTip.input);
+function totalBill
 
 
-
-
-}
-);
+button.addEventListener('click', () => {
+    let div = document.querySelector('#total');
+    div.innertext = bill.value;
+})
